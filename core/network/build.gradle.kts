fe66549bf93195d7b6df16ffbbe84d5d4ab18449
jhj1111 <happyijun@naver.com>
2026-03-22 02:21:13 +0900

    feat(secret): create secret.properties setting

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,10 +1,20 @@
 plugins {
     alias(libs.plugins.wavecast.android.library)
     alias(libs.plugins.wavecast.android.hilt)
+    alias(libs.plugins.secrets.gradle.plugin)
 }
 
 android {
     namespace = "com.example.wavecast.core.network"
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+}
+
+secrets {
+    propertiesFileName = "secret.properties"
 }
 
 dependencies {
