a8e0a72845c7046a4dca06e0b1db1321413deae9
jhj1111 <happyijun@naver.com>
2026-03-23 17:01:47 +0900

    fix(network): fix player NetworkOnMainThreadException error
    - 동기 방식 -> Dispatchers.IO에서 실행 방식으로 변경

--- core/network/src/main/java/com/example/wavecast/core/network/api/RssService.kt
+++ core/network/src/main/java/com/example/wavecast/core/network/api/RssService.kt
@@ -2,6 +2,8 @@ package com.example.wavecast.core.network.api
 
 import com.example.wavecast.core.network.model.EpisodeResponse
 import com.example.wavecast.core.network.utils.RssParser
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import javax.inject.Inject
@@ -12,13 +14,15 @@ class RssService @Inject constructor(
     private val okHttpClient: OkHttpClient,
     private val rssParser: RssParser
 ) {
-    suspend fun fetchEpisodes(url: String): List<EpisodeResponse> {
+    suspend fun fetchEpisodes(url: String): List<EpisodeResponse> = withContext(Dispatchers.IO) {
         val request = Request.Builder()
             .url(url)
             .build()
         
         val response = okHttpClient.newCall(request).execute()
-        return response.body?.byteStream()?.use { inputStream ->
+        if (!response.isSuccessful) return@withContext emptyList()
+
+        response.body?.byteStream()?.use { inputStream ->
             rssParser.parse(inputStream)
         } ?: emptyList()
     }
